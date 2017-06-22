ff968df11fee6cd59c3b55321f6b4e294c66ba99
miranghimire <070bct521@gmail.com>
2017-06-22 20:49:36 +0000

    Used Dill for dumping of hashtable

---
+++ web/pbmrs/static/pbmrs/js/graphapi.js
@@ -0,0 +1,70 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '432605433783166',
+      xfbml      : true,
+      version    : 'v2.9'
+    });
+	  FB.getLoginStatus(function(response){
+		  statusChangeCallback(response);
+	  });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response){
+	if(response.status == 'connected'){
+		console.log("logged in");
+		setElements(true);
+		testAPI();
+	}else {
+		console.log("logged out");
+		setElements(false);
+	}
+	logout.onclick = function(){
+		FB.logout(function(response){
+		setElements(false);
+		});
+	}
+}
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+function setElements(isLoggedIn){
+	if(isLoggedIn){
+		document.getElementById('profile').style.display='block';
+		document.getElementById('fb-btn').style.display='none';
+		document.getElementById('startMsg').innerHTML =""
+	}else{
+		document.getElementById('profile').style.display='none';
+		document.getElementById('fb-btn').style.display='block';
+		document.getElementById('startMsg').innerHTML ="Log in to determine your personality"
+		document.getElementById('state').innerHTML = "" 
+
+	}
+}
+
+function testAPI(){
+	FB.api('/me?fields=name,posts',function(response){
+		if(response && !response.error){
+			console.log(response)
+			buildProfile(response);
+		}
+	});
+}
+function buildProfile(user){
+	let profile = `<h3>Welcome ${user.name}</h3> 
+			<ul class ="list-group">
+			<li class="list-group-item">User ID: ${user.id}</li>
+			<li class="list-group-item">Latest Post: ${user.posts.data[0].story}</li>
+			</ul>
+	`;
+	document.getElementById('state').innerHTML = profile
+}
