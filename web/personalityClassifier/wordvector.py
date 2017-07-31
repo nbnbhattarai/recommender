b9c2132cc59db7f08a2bc0707cb5c8a5928a2abf
Abhishek Paudel <myheartswift@gmail.com>
2017-07-31 16:28:16 +0545

    wordvector and dataset modified

--- web/personalityClassifier/wordvector.py
+++ web/personalityClassifier/wordvector.py
@@ -2,10 +2,15 @@ import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
+
 from collections import Counter
+import csv
+import itertools as IT
+import operator
+
 
 def preprocessing():
-	train = pd.read_csv('trainSet.csv')
+	train = pd.read_csv('original_dataset.csv')
 	newdict = {'status':[],'o':[],'c':[],'e':[],'a':[],'n':[]}
 	train.sort_values(by="#AUTHID")
 	authid = ''
@@ -21,19 +26,26 @@ def preprocessing():
 			newdict['n'].append((row[9]))
 			authid = row[1]
 	df=pd.DataFrame(newdict)
-	df.to_csv('preprocessed_dataset.csv')
+	df.to_csv('original_preprocessed_dataset.csv')
 
 
 def makefeature():
-	data = pd.read_csv('preprocessed_dataset.csv')
+	data = pd.read_csv('original_preprocessed_dataset.csv')
 	porter = PorterStemmer()
 	stopW = (stopwords.words('english')) + ['.', '...', '?', '!', '/', '(', ')', '&', '%', '@', '#', ',', ';', ':', '=', '--', '[', ']']
 	goodword = []
 	vocab = []
 	for row in data['status']:
+		# print(row)
 		token = [porter.stem(i.lower()) for i in word_tokenize(row) if i.lower() not in stopW]
-		goodword.append(token)
-		vocab += token
+		pos = pos_tag(token)
+		tags = ['RB', 'RBR', 'RBS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'JJ', 'JJR','JJS']
+		word = []
+		for wpos in pos:
+			if wpos[1] in tags:
+				word.append(wpos[0])		
+		goodword.append(word)
+		vocab += word
 	document = list(vocab)
 	vocab_count = dict(Counter(vocab))
 	sorted_vocab = sorted(vocab_count.items(), key=operator.itemgetter(1))
@@ -45,10 +57,14 @@ def makefeature():
 	newdict = {'word_vector':[]}
 	for row in data.itertuples():
 		token = [porter.stem(i.lower()) for i in word_tokenize(row[7]) if i.lower() not in stopW]
-		vector_dict = dict(Counter(token))
+		pos = pos_tag(token)
+		word = []
+		for wpos in pos:
+			if wpos[1] in tags:
+				word.append(wpos[0])		
+		vector_dict = dict(Counter(word))
 		w = []
 		for i in vocab:
-			
 			if i in vector_dict:
 				w.append(vector_dict[i])
 			else:
@@ -56,6 +72,29 @@ def makefeature():
 		newdict['word_vector'].append(w)
 	df=pd.DataFrame(newdict)
 	# print(df)
-	df.to_csv('wordvector.csv')
+	df.to_csv('original_wordvector.csv')
 	return vocab
 
+# def combine_dataset():
+# 	filenames = ['original_preprocessed_dataset.csv', 'original_wordvector.csv']
+# 	handles = [open(filename, 'r') for filename in filenames]    
+# 	readers = [csv.reader(f, delimiter=',') for f in handles]
+
+# 	with  open('original_wordvector_final.csv', 'w') as h:
+# 	    writer = csv.writer(h, delimiter=',', lineterminator='\n', )
+# 	    for rows in IT.zip_longest(*readers, fillvalue=['']*2):
+# 	        combined_row = []
+# 	        for row in rows:
+# 	            row = row[:2] # select the columns you want
+# 	            if len(row) == 2:
+# 	                combined_row.extend(row)
+# 	            else:
+# 	                combined.extend(['']*2)
+# 	        writer.writerow(combined_row)
+
+# 	for f in handles:
+# 	    f.close()
+
+# preprocessing()
+makefeature()
+# combine_dataset()
